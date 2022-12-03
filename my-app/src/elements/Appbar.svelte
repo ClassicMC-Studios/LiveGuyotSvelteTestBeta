fa99e3200f41a53852cb7806aaa7741b9af80ba0
ClassicMC <classicmclive@gmail.com>
2022-12-03 17:15:45 +0000

    issue #2 fixed

--- my-app/src/elements/Appbar.svelte
+++ my-app/src/elements/Appbar.svelte
@@ -7,8 +7,8 @@
 <main>
 	<div class="bc-d_b bc-r-10 bc-border bc-bdc-white " style="margin:1%;">
 		&nbsp;<i class="material-icons">videogame_asset</i><br/>
-		&nbsp;<a href="https://classicmc-studios.github.io/guyot/games/spyro.html" class="bc-text-link ">Spyro Season of Flame__</a> <a href="https://classicmc-studios.github.io/guyot/games/yoshi.html" class="bc-text-link ">Yoshi's Island</a><br/>
-		&nbsp;<a href="https://classicmc-studios.github.io/guyot/games/ffv.html" class="bc-text-link ">Final Fantasy V__</a><a href="https://classicmc-studios.github.io/guyot/games/yo.html" class="bc-text-link "> Yoshi's Island ROM hack</a><br/>
+		&nbsp;<a href="https://classicmc-studios.github.io/guyot/games/spyro.html" class="bc-text-link ">Spyro Season of Flame__</a> <a href="https://classicmc-studios.github.io/guyot/games/yo.html" class="bc-text-link ">Yoshi's Island</a><br/>
+		&nbsp;<a href="https://classicmc-studios.github.io/guyot/games/ffv.html" class="bc-text-link ">Final Fantasy V__</a><a href="https://classicmc-studios.github.io/guyot/games/yoshi.html" class="bc-text-link "> Yoshi's Island ROM hack</a><br/>
 		&nbsp;<a href="https://classicmc-studios.github.io/guyot/games/emerald.html" class="bc-text-link ">Pokémon Emerald</a><wrap class="bc-text-link">__</wrap> <a href="https://classicmc-studios.github.io/guyot/games/smw.html"class="bc-text-link"> Super Mario World</a><br/>
 		&nbsp;<a href="https://classicmc-studios.github.io/guyot/games/leafgreen.html" class="bc-text-link ">Pokémon Leaf Green</a><br/>
 	</div>
